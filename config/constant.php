29ecd3092f901f3b5f6b7579b4e405b848be71af
unknown <admin@ERGODE-AN-124.ergodeit1.com>
2017-03-10 20:59:58 +0530

    [Ravinesh][Added Some File On Github]

---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+  $base_path = dirname(dirname(__FILE__));
+  define("EDI_BASE_PATH", $base_path);
+  define("XML_FILE_PATH", $base_path."/XML/");
+  define("EDI_FILE_PATH", $base_path."/EDI/");
+
+?>
